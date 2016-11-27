621ea1cf9b397a2b8cd9bc231b6f9ff036d2295c
Bryan Murray <bryannmurrayy@gmail.com>
2016-11-27 02:18:05 +0000

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :doman          => 'fast-cove-32163.herokuapp.com',
+    :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
