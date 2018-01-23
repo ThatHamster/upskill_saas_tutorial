a9199d48db6ea0779bb84dbfdc90f49b0cbf9101
Bryan Murray <bryannmurrayy@gmail.com>
2018-01-23 02:59:21 +0000

    Fixed sign up bugs and ui design

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,7 +10,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       if params[:plan]
         resource.plan_id = params[:plan]
         if resource.plan_id == 2
-          resource.save_with_subscription\
+          resource.save_with_subscription
         else
           resource.save
         end
